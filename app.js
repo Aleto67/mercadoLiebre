84fa4603a3770926be0269c76f6db41f59b60b02
Aleto67 <aleto67@gmail.com>
2022-10-18 16:15:55 -0300

    cambios a 18 10 22

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ app.listen(3000, () => console.log('Servidor levantado en el puerto 3000'));
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname,'views/home.html'))
 })
+
+app.post('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'views/home.html'))
+})
+
+app.get('/register', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'views/register.html'))
+})
+
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'views/login.html'))
+})
